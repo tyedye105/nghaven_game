ef732a954fad5b0d65a4a8afdefc19c13a306737
Marty Kovach and Maxwell Cady <marty+maxwell@>
2017-01-31 10:51:52 -0800

    initial branch commit

--- src/app/travel/travel.component.ts
+++ src/app/travel/travel.component.ts
@@ -72,7 +72,6 @@ export class TravelComponent implements OnInit {
         }
       }
 
-
   travelFunction() {
     var randomNumber = Math.floor(Math.random() * 17) + 1;
     this.travelEvents(randomNumber);
@@ -80,44 +79,3 @@ export class TravelComponent implements OnInit {
   }
 
 }
-
-
-//
-// if (randomNumber === 1) {
-//     this.playerService.increaseDaysRemaining(1);
-//   } else if (randomNumber === 2) {
-//       this.playerService.increaseDaysRemaining(2);
-//     } else if (randomNumber === 3) {
-//       this.playerService.increaseDaysRemaining(3);
-//     } else if (randomNumber === 4) {
-//       this.playerService.decreaseDaysRemaining(1);
-//     } else if (randomNumber === 5) {
-//       this.playerService.decreaseDaysRemaining(2);
-//     } else if (randomNumber === 6) {
-//       this.playerService.decreaseDaysRemaining(3);
-//     } else if (randomNumber === 7) {
-//       this.playerService.increaseCrystals(10);
-//     } else if (randomNumber === 8) {
-//       this.playerService.increaseCrystals(20);
-//     } else if (randomNumber === 9) {
-//       this.playerService.increaseCrystals(30);
-//     } else if (randomNumber === 10) {
-//       this.playerService.decreaseCrystals(10);
-//     } else if (randomNumber === 11) {
-//       this.playerService.decreaseCrystals(20);
-//     } else if (randomNumber === 12) {
-//       this.playerService.decreaseCrystals(30);
-//     } else if (randomNumber ===  13) {
-//       this.playerService.decreaseHp(10);
-//     } else if (randomNumber === 14) {
-//       this.playerService.decreaseHp(25);
-//     } else if (randomNumber === 15) {
-//       this.playerService.decreaseHp(50);
-//     } else if (randomNumber === 16) {
-//       this.playerService.increaseHp(10);
-//       this.playerService.increaseCrystals(10);
-//     } else {
-//       this.playerService.decreaseHp(10);
-//       this.playerService.decreaseCrystals(10);
-//     }
-//   }
