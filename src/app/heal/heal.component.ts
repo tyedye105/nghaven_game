4052c6dd0c49ec3c8506c69fef1f503bb2544afb
SuziRubino <suzi@suzirubi.com>
2017-01-30 20:45:48 -0800

    add switch method for events in heal, gather, rest and style buttons

--- src/app/heal/heal.component.ts
+++ src/app/heal/heal.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../player.model';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'heal',
   templateUrl: './heal.component.html',
-  styleUrls: ['./heal.component.scss']
+  styleUrls: ['./heal.component.scss'],
+  providers: [PlayerService]
 })
+
+
 export class HealComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
   }
+
+  healEvents(randomNumber) {
+    switch(randomNumber) {
+      case 1:
+        this.playerService.increaseDaysRemaining(1);
+        break;
+      }
+    }
+
 
 }
